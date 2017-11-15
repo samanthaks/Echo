082318dcdd7cdf16afc00c8d4822bbeb33394085
Sam Stultz <sks2200@columbia.edu>
2017-11-15 01:53:28 -0500

    update import os

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 db = SQLAlchemy()
 
